3348bf69271bbdf1217a34ae16f70d17ab7a4ea5
Teresia <teresiamwangi26@gmail.com>
2023-04-28 11:37:48 +0300

    Complete Assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
-}
+
+  const directorsName = directors.map((director, index) => {
+    return (
+      <div key={index}>
+        <h3>{director.name}</h3>
+        <ul>
+          {director.movies.map((movie, index) => {
+            return <li key={index}>{movie}</li>
+          })}
+        </ul>
+
+      </div>
+    )
+  })
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {directorsName}
+  </div>)
+};
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,28 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(movies);
+
+  const moviesList = movies.map((movie, index) => {
+    return (
+      <div key={index}>
+        <h2>{movie.title}</h2>
+        <h3>{movie.time} minutes</h3>
+        <ul>
+          {movie.genres.map((genre, index) => {
+            return <li key={index}>{genre}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {moviesList}
+    </div>
+  );
 }
 
 export default Movies;
